5f1a3d65786e6db1da00d2c7d4b45af0bd7eeed7
John Hurst <John_Hurst@genpt.com>
2024-06-14 08:32:47 -0400

    move completion install and config out of init

--- lua/custom/completion.lua
+++ lua/custom/completion.lua
@@ -1,62 +1,81 @@
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect', 'preview' }
-vim.opt.shortmess:append 'c'
-
-local lspkind = require 'lspkind'
-lspkind.init {}
-
+-- See `:help cmp`
 local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+luasnip.config.setup {}
 
 cmp.setup {
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'path' },
-    { name = 'buffer' },
-  },
-  mapping = {
-    ['<C-n>'] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
-    ['<C-p>'] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
-    ['<C-y>'] = cmp.mapping(
-      cmp.mapping.confirm {
-        behavior = cmp.ConfirmBehavior.Insert,
-        select = true,
-      },
-      { 'i', 'c' }
-    ),
-  },
-
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      luasnip.lsp_expand(args.body)
     end,
+  },
+  completion = { completeopt = 'menu,menuone,noinsert' },
+
+  -- For an understanding of why these mappings were
+  -- chosen, you will need to read `:help ins-completion`
+  --
+  -- No, but seriously. Please read `:help ins-completion`, it is really good!
+  mapping = cmp.mapping.preset.insert {
+    -- Select the [n]ext item
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- Select the [p]revious item
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+
+    -- Scroll the documentation window [b]ack / [f]orward
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
+    -- Accept ([y]es) the completion.
+    --  This will auto-import if your LSP supports it.
+    --  This will expand snippets if the LSP sent a snippet.
+    ['<C-y>'] = cmp.mapping.confirm { select = true },
+
+    -- If you prefer more traditional completion keymaps,
+    -- you can uncomment the following lines
+    --['<CR>'] = cmp.mapping.confirm { select = true },
+    --['<Tab>'] = cmp.mapping.select_next_item(),
+    --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+    -- Manually trigger a completion from nvim-cmp.
+    --  Generally you don't need this, because nvim-cmp will display
+    --  completions whenever it has completion options available.
+    ['<C-Space>'] = cmp.mapping.complete {},
+
+    -- Think of <c-l> as moving to the right of your snippet expansion.
+    --  So if you have a snippet that's like:
+    --  function $name($args)
+    --    $body
+    --  end
+    --
+    -- <c-l> will move you to the right of each of the expansion locations.
+    -- <c-h> is similar, except moving you backwards.
+    ['<C-l>'] = cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      end
+    end, { 'i', 's' }),
+    ['<C-h>'] = cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { 'i', 's' }),
+
+    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'path' },
   },
 }
 
--- setup vim-dadbod
-cmp.setup.filetype({ 'sql' }, {
-  sources = {
-    { name = 'vim-dadbod-completion ' },
-    { name = 'buffer' },
-  },
-})
+-- cmp.setup.filetype({ 'sql' }, {
+--   sources = {
+--     { name = 'vim-dadbod-completion' },
+--     { name = 'buffer' },
+--   },
+-- })
 
--- any file ending w/ lua in the snippets folder will get loaded as
--- custom snippets for luasnip
-for _, ft_path in ipairs(vim.api.nvim_get_runtime_file('lua/custom/snippets/*.lua', true)) do
-  loadfile(ft_path)()
-end
-
--- allows us to walk forward through
--- the set of "inputs" that a snippet defines
-vim.keymap.set({ 'i', 's' }, '<c-k>', function()
-  if ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  end
-end, { silent = true })
-
--- allows us to walk backward through
--- the set of "inputs" that a snippet defines
-vim.keymap.set({ 'i', 's' }, '<c-j>', function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
+-- require('luasnip.loaders.from_lua').load { paths = { './lua/custom/snippets/' } }
+require('luasnip.loaders.from_vscode').load { paths = { './lua/custom/snippets' } }
