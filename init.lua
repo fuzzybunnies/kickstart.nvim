140b6d18227f44d25a1c66a5e64728347fc2fa6e
John Hurst <John_Hurst@genpt.com>
2024-07-23 13:21:03 -0400

    configure lualine a little (no branch, change theme)

--- init.lua
+++ init.lua
@@ -818,7 +818,20 @@ require('lazy').setup({
   {
     'nvim-lualine/lualine.nvim',
     config = function()
-      require('lualine').setup {}
+      require('lualine').setup {
+        options = {
+          theme = 'powerline_dark',
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          -- lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_b = { 'diff', 'diagnostics' },
+          lualine_c = { 'filename' },
+          lualine_x = { 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' },
+        },
+      }
     end,
   },
 
