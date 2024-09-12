a8cedaa2c2bc7c5fd17cdbfe123a9390e1745307
John Hurst <John_Hurst@genpt.com>
2024-09-12 09:55:26 -0400

    Change colors for active/inactive for 'c' section

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -2,15 +2,6 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function()
     require('lualine').setup {
-      options = {
-        theme = function()
-          local custom_theme = require 'lualine.themes.powerline_dark'
-
-          custom_theme.inactive.c = { fg = '#DFFF00', bg = '#4287f5' }
-          custom_theme.normal.c = { bg = '#4da4b0' }
-          return custom_theme
-        end,
-      },
       sections = {
         lualine_a = { 'mode' },
         -- lualine_b = { 'branch', 'diff', 'diagnostics' },
@@ -19,6 +10,15 @@ return {
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
+      },
+      options = {
+        theme = function()
+          local custom_theme = require 'lualine.themes.powerline_dark'
+
+          custom_theme.inactive.c = { fg = '#DFFF00', bg = '#4287f5' }
+          custom_theme.normal.c = { fg = '#11110e', bg = '#4da4b0' }
+          return custom_theme
+        end,
       },
     }
   end,
