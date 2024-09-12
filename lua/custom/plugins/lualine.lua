9520ff678c8d35792d278c549829e7815cdd5fe6
John Hurst <John_Hurst@genpt.com>
2024-09-12 10:04:38 -0400

    Full path in filename

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -6,7 +6,9 @@ return {
         lualine_a = { 'mode' },
         -- lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_b = { 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
+        lualine_c = {
+          { 'filename', path = 3 },
+        },
         lualine_x = { 'filetype' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
