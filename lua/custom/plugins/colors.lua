7de0394e05e27b50159ac3b02641223e5d3da5df
John Hurst <John_Hurst@genpt.com>
2024-10-22 10:32:06 -0400

    Pulled the colorscheme stuff out to its own file. Added the evergarden colorscheme

---
+++ lua/custom/plugins/colors.lua
@@ -0,0 +1,36 @@
+return {
+  -- colorschemes
+  --
+  -- The following section is loading and configuring a colorscheme
+  {
+    'comfysage/evergarden',
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      contrast_dark = 'hard', -- 'hard'|'medium'|'soft'
+      overrides = {},
+    },
+  },
+  --
+  -- The following section is loading and configuring a colorscheme
+  -- AND during initialization is setting the colorscheme
+  --
+  { -- You can easily change to a different colorscheme.
+
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+}
