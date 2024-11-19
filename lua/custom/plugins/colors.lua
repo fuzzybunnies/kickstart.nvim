ad8c3f427ac98ad34e834b603cefc3a4fa8d97d2
John Hurst <John_Hurst@genpt.com>
2024-11-19 09:49:00 -0500

    Change default colorscheme

--- lua/custom/plugins/colors.lua
+++ lua/custom/plugins/colors.lua
@@ -10,6 +10,15 @@ return {
       contrast_dark = 'hard', -- 'hard'|'medium'|'soft'
       overrides = {},
     },
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'evergarden'
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
   },
   --
   -- The following section is loading and configuring a colorscheme
@@ -23,14 +32,5 @@ return {
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
   },
 }
